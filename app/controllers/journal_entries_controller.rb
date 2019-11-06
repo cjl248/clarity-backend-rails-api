90c8fdff57c6677dfd5844a94a2687367c6e5f8c
github cjl248 <github cjl248@cornell.edu>
2019-11-06 10:58:03 -0500

    journal_entries#update

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -8,6 +8,28 @@ class JournalEntriesController < ApplicationController
     render json: journal_entries.as_json(options)
   end
 
+  def show
+    journal_entry = JournalEntry.find(params[:id])
+    options ={
+      except: [:created_at, :updated_at]
+    }
+    render json: journal_entry.as_json(options)
+  end
+
+  def update
+    journal_entry = JournalEntry.find(params[:id])
+    byebug
+    journal_entry.update(journal_entry_params)
+    if journal_entry.valid?
+      options = {
+        except: [:created_at, :updated_at]
+      }
+      render json: journal_entry.as_json(options)
+    else
+      render json: {errors: journal_entry.errors.full_messages}
+    end
+  end
+
   def create
     journal_entry = JournalEntry.create(journal_entry_params)
     if journal_entry.valid?
@@ -22,7 +44,7 @@ class JournalEntriesController < ApplicationController
 
   def destroy
     journal_entry = JournalEntry.find(params[:id])
-    journal_entry.destroy()
+    journal_entry.destroy
     render json: {message: "#{journal_entry.title} was destroyed"}
   end
 
@@ -31,5 +53,6 @@ class JournalEntriesController < ApplicationController
   def journal_entry_params
     params.require(:journal_entry).permit(:user_id, :title, :date, :content)
   end
+
 
 end
